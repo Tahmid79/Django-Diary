844933046e5715cc7fe480f748ed4291d90aca2b
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-01-28 21:07:19 +0800

    Initial commit

---
+++ entries/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import  Entry
+
+# Create your views here.
+def index(request):
+    entries = Entry.objects.order_by('-date_posted')
+    context = {'entries': entries}
+    return render(request , 'entries/index.html', context)
+
+def add(request):
+    return render(request , 'entries/add.html')
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Jetbrains\PyCharm Projects\Diary\1. Create Project and App\1. Create Project and App\dear_diary\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
