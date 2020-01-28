13f3e2029cc3758318aaae708118aad6766b3f34
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-01-28 21:41:39 +0800

    Form Added

--- entries/views.py
+++ entries/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import  Entry
 from .forms import EntryForm
 
@@ -9,7 +9,16 @@ def index(request):
     return render(request , 'entries/index.html', context)
 
 def add(request):
-    form = EntryForm()
+    if request.method =='POST':
+        form = EntryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
+    else:
+        form = EntryForm()
 
     context = {'form' : form}
 
